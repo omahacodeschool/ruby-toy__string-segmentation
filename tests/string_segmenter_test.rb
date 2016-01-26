6ec424792cbe1cf3427ee6388e642743684f3d83
Michael Meier <mmeier61@yahoo.com>
2016-01-26 12:55:25 -0600

    WIP

--- tests/string_segmenter_test.rb
+++ tests/string_segmenter_test.rb
@@ -2,7 +2,16 @@ require 'test_helper'
 require 'string_segmenter'
 
 class StringSegmenterTest < Minitest::Test
-  # Your test methods go here.
+  def test_strings_full_words
+  str = "avacadoburritocatdog"
+  assert_equal(["avacado", "burrito", "cat", "dog"], string_segmenter(str))
+  end
+  def test_strings_other_characters
+  str = "avacad-o-burrito*catdog*"
+  assert_equal(["avacado", "burrito","cat","dog"], string_segmenter(str))
+  end
+  def test_full_words
+  str = "hampsterslegomantrouble"
+  assert_equal(["hampsters","lego","man","trouble"], string_segmenter(str))
+  end
 end
-
-# Remember, the names of all test methods should begin with "test_".
