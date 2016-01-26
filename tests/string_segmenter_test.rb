397f63e7637d17410ca9a23d26ae52300ac09bc1
Melinda Deinert <melinda.deinert@gmail.com>
2016-01-26 11:08:27 -0600

    String Segmenter Test

--- tests/string_segmenter_test.rb
+++ tests/string_segmenter_test.rb
@@ -2,7 +2,13 @@ require 'test_helper'
 require 'string_segmenter'
 
 class StringSegmenterTest < Minitest::Test
-  # Your test methods go here.
-end
+  def test_string_segmenter
+    sample_input = "turtlebluetraintrust"
+    assert_equal(["turtle", "blue", "train", "trust"])
+  end
 
-# Remember, the names of all test methods should begin with "test_".
+  def test_string_plurals
+    sample_input = "smilesbirthfive"
+    assert_equal(["smiles", "birth", "five"])
+  end
+end
