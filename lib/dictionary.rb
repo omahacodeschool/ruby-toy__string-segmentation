57097464956b004f1fcdbfce198b4934c26d183c
Shannon Jackson <shannonfromomaha@gmail.com>
2016-01-26 12:55:41 -0600

    test_of_tests

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,7 +1,2 @@
 def valid_word?(word)
-  # Populate this array with some words of your choosing.
-  dictionary = [butts]
-
-  # Return true if your dictionary contains `word`
-  # or false if it does not.
 end
