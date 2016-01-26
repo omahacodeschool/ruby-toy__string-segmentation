b798fb462797880cf74ce25f194112c54b118f07
Kyle Sandage <ksandage08@gmail.com>
2016-01-26 12:56:26 -0600

    Test Progress

--- tests/dictionary_test.rb
+++ tests/dictionary_test.rb
@@ -2,7 +2,19 @@ require 'test_helper'
 require 'dictionary'
 
 class DictionaryTest < Minitest::Test
-  # Your test methods go here.
+  def test_dictionary_if_true
+    sample_word = "words"
+    result = valid_word?(sample_word)
+
+    assert_equal(true, result)
+  end
+
+  def test_dictionary_if_false
+    sample_word = "dog"
+    result = valid_word?(sample_word)
+
+    assert_equal(false, result)
+  end 
 end
 
 # Remember, the names of all test methods should begin with "test_".
--- tests/string_segmenter_test.rb
+++ tests/string_segmenter_test.rb
@@ -2,7 +2,14 @@ require 'test_helper'
 require 'string_segmenter'
 
 class StringSegmenterTest < Minitest::Test
-  # Your test methods go here.
+  def test_find_word
+    sample_str = "turtlehappy"
+    assert_equal("turtle", segment_string(sample_str))
+  end
+  def test_plural_words
+    sample_str = "wordsturtle"
+    assert_equal("words", segment_string(sample_str))
+  end
 end
 
 # Remember, the names of all test methods should begin with "test_".
