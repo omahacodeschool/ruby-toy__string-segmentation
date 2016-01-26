2d7d3e017b8ee74ec6fe4e6d7c26b8529fd70bf5
Amy Wall <amymariewall@gmail.com>
2016-01-26 14:41:55 -0600

    TIAS

--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -1,5 +1,24 @@
 require 'dictionary'
 
 def segment_string(str)
+  letters=str.split""
+  letters.each do |x, i|
+    word=""
+    i+=1
+    word << letters[i+=1] until valid_word?(word) == true
+str.index+=1
+valid_word?(letters[0]+letters[1])==true
+
+str.each_char do |x|
+  word_string = str[0..x.index-1] << x
+  if valid_word?(word_string) == true
+    result_array = []
+    result_array << word_string
+
+string[0] << x
+
+string[0..x.index-1] 
+
+until str[0..string.index(x)] 
 
 end
