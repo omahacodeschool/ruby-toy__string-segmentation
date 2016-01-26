92a94d1dca949a823483988595b66e29448174fc
Jeremy Stoll <j8r8my@gmail.com>
2016-01-26 13:46:46 -0600

    Progress

--- tests/string_segmenter_test.rb
+++ tests/string_segmenter_test.rb
@@ -4,17 +4,17 @@ require 'string_segmenter'
 class StringSegmenterTest < Minitest::Test
   def test_1
     string = "diamondbluecrystalquantitygorilla"
-    assert_equal("diamond blue crystal quantity gorilla", segment_string(string))
+    assert_equal(["diamond", "blue", "crystal", "quantity", "gorilla"], segment_string(string))
   end
   
   def test_2
     string = "diamondbluestcrystalquantitygorilla" 
-    assert_equal("diamond bluest crystal quantity gorilla", segment_string(string))
+    assert_equal(["diamond", "bluest", "crystal", "quantity", "gorilla"], segment_string(string))
   end
-  
+
   def test_3
     string = "diamondbluestcrystalizequantitygorilla" 
-    assert_equal("diamond bluest crystalize quantity gorilla", segment_string(string)) # Your test methods go here.
+    assert_equal(["diamond", "bluest", "crystalize", "quantity", "gorilla"], segment_string(string)) # Your test methods go here.
 end
 end
 
