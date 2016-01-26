49f8a9954dc1c1b2698c2b33cbcb2984a53c77b9
Courtney Burns <courtneylynnburns@gmail.com>
2016-01-26 14:38:27 -0600

    tests

--- tests/string_segmenter_test.rb
+++ tests/string_segmenter_test.rb
@@ -2,7 +2,14 @@ require 'test_helper'
 require 'string_segmenter'
 
 class StringSegmenterTest < Minitest::Test
-  # Your test methods go here.
-end
+  def test_string_with_capital_letters
+    str = "IWouldBuyYouAnExoticPetLikeALlamaOrAnEmu"
+    assert_equal(["I","Would","Buy","You","An","Exotic","Pet","Like","A","Llama","Or","An","Emu"], segment_string(str))
+  end
 
+class StringSegmenterTest < Minitest::Test
+  def test_string_with_plural_words
+    str = "newkidsontheblockhadabunchofhitschinesefoodmakesmesickIthinkitisflywhengirlsstopbyforthesummerforthesummer"
+    assert_equal(["new","kids","on","the","block","had","a","bunch","of","hits","chinese","food","makes","me","sick","I","think","it","is","fly","when","girls","stop","by","for","the","summer","for","the","summer"], segment_string(str))
+  end
 # Remember, the names of all test methods should begin with "test_".
