ab23bb16d4a5d738063d12f70c2efb4d25df43a9
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-01-26 13:39:54 -0600

    Tests_done?

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,6 +1,6 @@
 def valid_word?(word)
   # Populate this array with some words of your choosing.
-  dictionary = []
+  dictionary = ["one","two","three","ones","twos","threes","opening","closing","ending","starting","connection","rejection","abstratction","affection","trusted","fated","parted","walked","cement","casement","basement","pavement","lovely","carefully","originally","positive","suggestive","connective","judgingly","endings","aliases","basements"]
 
   # Return true if your dictionary contains `word`
   # or false if it does not.
