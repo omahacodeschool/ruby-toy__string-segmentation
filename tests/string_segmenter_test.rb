ad9b780dd2c577f398ac8629a7a55ca8a5e91b68
Bradley Biehl <bradley.biehl@gmail.com>
2016-01-26 14:37:41 -0600

    string_segmenter_test

--- tests/string_segmenter_test.rb
+++ tests/string_segmenter_test.rb
@@ -5,42 +5,12 @@ class StringSegmenterTest < Minitest::Test
   # Your test methods go here.
   def test_string_segmentation_basic
     sample_string = "onetwothree"
-    assert_equal("one, two, three", segment_string(sample_string))
+    assert_equal("one", "two", "three", segment_string("sample_string"))
   end
 
-  def test_string_segmentation_plurals
-    sample_string = "onestwosthrees"
-    assert_equal("ones, twos, threes", segment_string(sample_string))
-  end
-
-  def test_string_segmentation_ing
-    sample_string = "openingclosingendingstarting"
-    assert_equal("opening, closing, ending, starting", segment_string(sample_string))
-  end
-
-  def test_string_segmentation_ion
-    sample_string = "connectionrejectionabstractionaffection"
-    assert_equal("connection, rejection, abstraction, affection", segment_string(sample_string))
-  end
-
-  def test_string_segmentation_ed
-    sample_string = "trustedfatedpartedwalked"
-    assert_equal("trusted, fated, parted, walked", segment_string(sample_string))
-  end
-
-  def test_string_segmentation_ment
-    sample_string = "cementcasementbasementpavement"
-    assert_equal("cement, casement, basement, pavement", segment_string(sample_string))
-  end
-
-  def test_string_segmentation_ly
-    sample_string = "lovelycarefullyoriginally"
-    assert_equal("lovely, carefully, originally", segment_string(sample_string))
-  end
-
-  def test_string_segmentation_ive
-    sample_string = "positivenegativeconnective"
-    assert_equal("positive, negative, connective", segment_string(sample_string))
+  def test_string_segmentation_anchorman_intro
+    sample = "hownowbrowncow"
+    assert_equal("how", "now", "brown", "cow", segment_string("sample_string"))
   end
 
 end
