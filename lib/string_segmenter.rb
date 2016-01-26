dc3d8ed99f58af3d9e45452a79c74b5b084d139b
Michael Meier <mmeier61@yahoo.com>
2016-01-26 13:34:30 -0600

    WIP

--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -1,2 +1,10 @@
+require 'dictionary'
 def segment_string(str)
+  words = []
+  str.downcase.delete('^a-z ')
+  if str.include dictionary
+    then words(//) << str
+  end
 end
+segment_string("avacadobuscatdog")
+words
--- tests/dictionary_test.rb
+++ tests/dictionary_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 require 'dictionary'
 
 class DictionaryTest < Minitest::Test
-  # Your test methods go here.
+  def test_word_capable
+  str = "avacado"
+  assert_equal(True, dictionary(word)
 end
-
-# Remember, the names of all test methods should begin with "test_".
