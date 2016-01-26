4635acc1529385520370f5150a935695b3b29c89
Courtney Burns <courtneylynnburns@gmail.com>
2016-01-26 17:44:11 -0600

    tests

--- tests/string_segmenter_test.rb
+++ tests/string_segmenter_test.rb
@@ -3,14 +3,14 @@ require 'string_segmenter'
 
 class StringSegmenterTest < Minitest::Test
   def test_string_with_capital_letters
-    str = "IWouldBuyYouAnExoticPetLikeALlamaOrAnEmu"
-    assert_equal(["I","Would","Buy","You","An","Exotic","Pet","Like","A","Llama","Or","An","Emu"], segment_string(str))
+    str = "CatcallErgo"
+    assert_equal(["Catcall","Ergo"], segment_string(str))
   end
 
 class StringSegmenterTest < Minitest::Test
   def test_string_with_plural_words
-    str = "newkidsontheblockhadabunchofhitschinesefoodmakesmesickIthinkitisflywhengirlsstopbyforthesummerforthesummer"
-    assert_equal(["new","kids","on","the","block","had","a","bunch","of","hits","chinese","food","makes","me","sick","I","think","it","is","fly","when","girls","stop","by","for","the","summer","for","the","summer"], segment_string(str))
+    str = "catcallsergo"
+    assert_equal(["catcalls","ergo"], segment_string(str))
   end
 end
 end
