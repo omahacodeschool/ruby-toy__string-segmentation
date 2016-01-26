e4b246d387583fa842e985190b11418ece714efe
Melinda Deinert <melinda.deinert@gmail.com>
2016-01-26 12:06:06 -0600

    Tests

--- tests/string_segmenter_test.rb
+++ tests/string_segmenter_test.rb
@@ -4,11 +4,11 @@ require 'string_segmenter'
 class StringSegmenterTest < Minitest::Test
   def test_string_segmenter
     sample_input = "turtlebluetraintrust"
-    assert_equal(["turtle", "blue", "train", "trust"])
+    assert_equal(["turtle", "blue", "train", "trust"], segment_string(sample_input))
   end
 
-  def test_string_plurals
+  def test_string_segmenter_plurals
     sample_input = "smilesbirthfive"
-    assert_equal(["smiles", "birth", "five"])
+    assert_equal(["smiles", "birth", "five"], segment_string(sample_input))
   end
 end
