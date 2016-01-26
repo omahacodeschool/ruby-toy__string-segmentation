3d2e0fadac641cf9bf3c9eb4548084dbb047245f
Bradley Biehl <bradley.biehl@gmail.com>
2016-01-26 13:48:10 -0600

    string_segmenter_test

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,6 +1,6 @@
 def valid_word?(word)
   # Populate this array with some words of your choosing.
-  dictionary = []
+  dictionary = %w(turtle train trust blue)
 
   # Return true if your dictionary contains `word`
   # or false if it does not.
