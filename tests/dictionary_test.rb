2b17c96eb2e2b74b570179643b20b53437f6411c
Ryan Newell <youcatastrophe@gmail.com>
2016-01-26 13:48:51 -0600

    Tests should be done

--- tests/dictionary_test.rb
+++ tests/dictionary_test.rb
@@ -2,7 +2,14 @@ require 'test_helper'
 require 'dictionary'
 
 class DictionaryTest < Minitest::Test
-  # Your test methods go here.
+  def test_in_Dictionary?
+  sample_word = "dinosaur"
+  assert_equal(true, valid_word?(sample_word))
+  end
+  def not_in_Dictionary?
+  sample_word = "notaword"
+  assert_equal(false, valid_word?(sample_word))
+  end
 end
 
 # Remember, the names of all test methods should begin with "test_".
--- tests/string_segmenter_test.rb
+++ tests/string_segmenter_test.rb
@@ -2,7 +2,30 @@ require 'test_helper'
 require 'string_segmenter'
 
 class StringSegmenterTest < Minitest::Test
-  # Your test methods go here.
+  def test_segment_string
+    string = "dinosaurhobbitbalrog"
+    assert_equal(["dinosaur", "hobbit", "balrog"], segment_string(string))
+  end
+  def test_ings
+    string = "roaringeatingburning"
+    assert_equal(["roaring", "eating", "burning"], segment_string(string))
+  end
+  def test_plurals
+    string = "dinosaurshobbitsbalrogs"
+    assert_equal(["dinosaurs", "hobbits", "balrogs"], segment_string(string))
+  end
+  def test_eds
+    string = "roaredsmokedburned"
+    assert_equal(["roared", "smoked", "burned"], segment_string(string))
+  end
+  def test_ly
+    string = "lovelyhighlystupidly"
+    assert_equal(["lovely", "highly", "stupidly"], segment_string(string))
+  end
+  def test_ment
+    string = "developmentshipmentmovement"
+    assert_equal(["development", "shipment", "movement"], segment_string(string))
+  end
 end
 
-# Remember, the names of all test methods should begin with "test_".
+#Remember, the names of all test methods should begin with "test_".
