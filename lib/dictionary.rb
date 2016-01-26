1eda3e0779205ec3b1c3d99365766d30e7c33ef3
Shannon Jackson <shannonfromomaha@gmail.com>
2016-01-26 11:00:29 -0600

    string_segmentation_test

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,6 +1,6 @@
 def valid_word?(word)
   # Populate this array with some words of your choosing.
-  dictionary = []
+  dictionary = [butts]
 
   # Return true if your dictionary contains `word`
   # or false if it does not.
