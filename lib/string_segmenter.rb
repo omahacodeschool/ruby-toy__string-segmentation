27911b6afa179f529535d0d923d6a90ab30a8354
Melinda Deinert <melinda.deinert@gmail.com>
2016-01-27 09:38:40 -0600

    Skip Counter ??

--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -10,7 +10,18 @@ def segment_string(str)
     if valid_word?(word) == true
       wordArray.push(word)
       collect = []
-    end
+    when i == collect[-1]
+      if collect.empty? == false
+        str.each do |i| #skip counter += 1 here?
+          collect.push(i)
+          word = collect.join('')
+          if valid_word?(word) == true
+            wordArray.push(word)
+            collect = []
+          end
+        end
+      else break
+      end
   end
   return wordArray
 end
