7bf8c03be7bead66dd42ba1870dec1b1e3e7d8aa
Shannon Jackson <shannonfromomaha@gmail.com>
2016-01-27 16:20:59 -0600

    wip_needstoloop

--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -1,37 +1,48 @@
 require_relative 'dictionary'
 
 def segment_string(str)
+  puts "STARTING!"
+
   characters = str.to_s.split("")
+  puts "characters are #{characters}"
+
   char_evaluation = []
   possible_words = []
-  answer = []
   skip_counter = 0
+
+  puts "About to loop..."
   until characters.empty?
     char_evaluation << characters.shift
+
+    puts "Just shifted characters."
+    puts "Now char_evaluation is #{char_evaluation}"
+    puts "and characters is #{characters}"
+
     test = char_evaluation.join
-    if valid_word?(test) == true  
-      possible_words<< test.dup
+    if valid_word?(test) == true
+     puts "THATS A WORD" 
+      possible_words<< char_evaluation.join
       char_evaluation = []
-      leftovers = characters.dup
     end
-    if valid_word?(test) == true && skip_counter != 0
+
+    if valid_word?(test) == false && characters.empty?
+    
+    puts "NOW THIS IS RELEVENT"
+      
+      skip_counter = skip_counter + 1
       x = skip_counter
-      until leftovers.empty?
-        possible_words[x].to_s.split""<< leftovers.shift
-        test = possible_words.join
-        if valid_word?(test) == true  
-        possible_words<< test.dup
-        char_evaluation = []
-        leftovers = characters.dup
-        end
+      possible_words[x]<< char_evaluation.shift
+      puts "possible_words is now #{possible_words}" 
+      test = possible_words.join
+      if valid_word?(test) == true  
+      possible_words<< char_evaluation.join
+      char_evaluation = []
       end
+      puts "DID IT"
     end
-    if leftovers.nil?
-      puts "yay"
-      else skip_counter = +1
-      end
-    end
-    return possible_words
   end
 
-puts segment_string("catyarnpoppertrashhamper")
+  return possible_words
+end
+
+puts segment_string("catdoghamper")
