1ab37f6354bfa69499223692eb5e5021a06db1b6
Courtney Burns <courtneylynnburns@gmail.com>
2016-01-27 10:59:37 -0600

    the stupid tests work now

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -2,7 +2,7 @@ def valid_word?(word)
   # Populate this array with some words of your choosing.
   dictionary = ["a","are","you","for","real","yo","cats","go","cat","rain","bow","rainbow","salt","water","saltwater"]
 
-  if dictionary.include?(word)
+  dictionary.include?(word)
 end  
   # this method takes any input(word) and compares to anything in the dictionary array 
 
--- tests/dictionary_test.rb
+++ tests/dictionary_test.rb
@@ -2,6 +2,15 @@ require 'test_helper'
 require 'dictionary'
 
 class DictionaryTest < Minitest::Test
-    
+  def test_dictionary_true
+      word="real"
+      assert_equal(true, valid_word?(word))
+  end
+  def test_dictionary_false
+      word="rainvow"
+      assert_equal(false, valid_word?(word))
+  end
+#test the method with a word that is included - should return true
+#test with a word that is not included - should return false
 end 
 # Remember, the names of all test methods should begin with "test_".
