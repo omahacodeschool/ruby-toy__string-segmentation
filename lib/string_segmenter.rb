819ccbbf050b5db8af081030c8b71299b8d4e492
Derek Pressnall <derekpressnall@yahoo.com>
2016-01-26 23:56:36 -0600

    WIP

--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -2,16 +2,23 @@ require_relative 'dictionary'
 
 def segment_string(str)
   characters = str.chars
-  new_word = []
-  word_arr = []
+  new_word = [ ]
+  word_arr = [ ]
   
-  new_word << characters.shift
-  valid_word?(new_word.join) == true
+  characters.each do |x|
+  new_word << x
+if valid_word?(new_word.join) == true
   word_arr << new_word.join
-  new_word = []
+  new_word = [ ]
   return word_arr
-
+  end
+end
 end
 
 segment_string('iloveyou')
+
+#my_array.each_with_index do |item, index|
+    #if index == 0
+        # do something with the first item
+    #end
 
