77328480145b481efc99edc27a1ad91a79ca5f98
Courtney Burns <courtneylynnburns@gmail.com>
2016-01-27 16:06:37 -0600

    string code

--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -1,46 +1,15 @@
 def segment_string(str)
-  # Loop through string until all groups of letters match a dictionary entry with no dangling letters
-  # Dictionary= a, are, you , for, real, yo
-  # new=[]
-  # Str=areyouforreal
+Dictionary=["cat", "jog"]
+str="jogcat"
+Final=[]
+puts Final[]
+i=0
 
-  # is "a" a match to the dictionary? -t - hold it? move out to new array?
-  # move to next element
-  # is "r" a match to the dictionary?-f
-  # re-f
-  # ref-f
-  # refo-f
-  # refor-f
-  # reforr-f
-  # reforre-f
-  # reforrea-f
-  # reforreal-f
-  # End of string
-  # Is Array empty?=has dangling letters?-t
-  # If true bring last word moved back to original array to original spot
-  # Start at end of last word moved and add next letter
-  # is ar a match to the dictionary-f
-  # are-t-hold
-  # y-f
-  # yo-t-hold
-  # u-f
-  # uf-f
-  # ufor-f
-  # uforr-f
-  # uforre-f
-  # uforrea-f
-  # uforreal-f
-  # end of string. 
-  # go back to last true statement and add next letter
-  # you-t
-  # f-f
-  # fo-f
-  # for-t
-  # r-f
-  # re-f
-  # rea-f
-  # real-t-hold
-  # end of string
-
+x=str.slice(0..i+=1)
+  Until True 
+  dictionary.include?(x) 
+  if true 
+    Final << str.slice(0,current itteration)
+    Return Final
 
 end
