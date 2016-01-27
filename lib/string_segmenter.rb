91a25229743d40765e519f284c7661a71885ab3d
Shannon Jackson <shannonfromomaha@gmail.com>
2016-01-26 20:43:48 -0600

    wip_segmentation_simple

--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -1,2 +1,21 @@
+require_relative 'dictionary'
+
 def segment_string(str)
+  letters = str.to_s.split("")
+  words = []
+  answer = []
+  while letters.length > 0
+    words << letters.shift
+    test = words.join
+    if valid_word?(test) == true
+      answer << words.join
+      words = []
+      puts answer.to_s + "= answer"
+      puts words.to_s + "= words"
+      puts letters.to_s + "= letters"
+
+    end
+  end
 end
+
+segment_string("catpopper")
