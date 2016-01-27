26acf8345c310102a1219df47f4e685b05e6eda4
Derek Pressnall <derekpressnall@yahoo.com>
2016-01-27 17:02:11 -0600

    psuedo code

--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -2,43 +2,45 @@ require_relative 'dictionary'
 
 def segment_string(str)
   characters = str.chars
-  puts "characters are now #{characters}"
-
   characters.length
   new_word = [ ]
   collected_words = { }
   i = 0
-  danglers = i
-  skip_counter = 0
+  #skip_counter = 0
 
   
   while characters.length > i 
     new_word << characters.shift
       if valid_word?(new_word.join) == true
-        #collected_words.inject(new_word.join)  #figure out how to store in hash
-          if danglers = false
-          new_word = [ ]
-            if danglers = true 
-              skip_counter = 1
-              erase the the dangling characters
-                call the key of the last word
-                  while characters.length > i 
-                    new_word << characters.shift
-                      if valid_word?(new_word.join) == true
-                        collected_words.inject(new_word.join)
-                          if word is already in hash reset skip_counter 
-                          end
-                      end
-                  end
-            end
-          end
+        collected_words[new_word] = i
+        new_word = [ ]
       end
-  end    
+  end
+          
   return collected_words
   
 end
 
-segment_string('ilovepuzzles')
+puts segment_string('ilovepuzzles')
 
 
 
+          if valid_word?(new_word.join) == false
+                danglers << new_word
+                  if danglers == true 
+                    skip_counter + 1
+                    erase the the dangling characters #slice?
+                    call the key of the last word
+                      while characters.length > i 
+                        new_word << characters.shift
+                          if valid_word?(new_word.join) == true
+                            collected_words.inject(new_word.join)
+                              if word is already in hash reset skip_counter 
+                              end
+                          end
+                      end
+                  end
+              end
+          end
+      end
+  end  
