279553b4219e28665f07530a5999653a78acbd7a
Derek Pressnall <derekpressnall@yahoo.com>
2016-01-27 16:06:38 -0600

    skeleton idea

--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -2,26 +2,43 @@ require_relative 'dictionary'
 
 def segment_string(str)
   characters = str.chars
+  puts "characters are now #{characters}"
+
+  characters.length
   new_word = [ ]
-  word_arr = [ ]
+  collected_words = { }
+  i = 0
+  danglers = i
+  skip_counter = 0
+
   
-#until characters.length  
-  characters.each do |x|
-  new_word << x
-if valid_word?(new_word.join) == true
-  word_arr << new_word.join 
-  new_word = [ ]
-  
-end
-end  
-  return word_arr
+  while characters.length > i 
+    new_word << characters.shift
+      if valid_word?(new_word.join) == true
+        #collected_words.inject(new_word.join)  #figure out how to store in hash
+          if danglers = false
+          new_word = [ ]
+            if danglers = true 
+              skip_counter = 1
+              erase the the dangling characters
+                call the key of the last word
+                  while characters.length > i 
+                    new_word << characters.shift
+                      if valid_word?(new_word.join) == true
+                        collected_words.inject(new_word.join)
+                          if word is already in hash reset skip_counter 
+                          end
+                      end
+                  end
+            end
+          end
+      end
+  end    
+  return collected_words
   
 end
 
-segment_string('iloveyou')
+segment_string('ilovepuzzles')
 
-#my_array.each_with_index do |item, index|
-    #if index == 0
-        # do something with the first item
-    #end
+
 
