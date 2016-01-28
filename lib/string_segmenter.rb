2ae321d8b2c913df55a136f0b71d711c31da7c81
Derek Pressnall <derekpressnall@yahoo.com>
2016-01-27 18:54:48 -0600

    getting one word with hash index

--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -3,18 +3,21 @@ require_relative 'dictionary'
 def segment_string(str)
   characters = str.chars
   characters.length
-  new_word = [ ]
-  collected_words = { }
+  word_build = [ ]
+  collected_words = [ ]
   i = 0
   #skip_counter = 0
 
-  
-  while characters.length > i 
-    new_word << characters.shift
-      if valid_word?(new_word.join) == true
-        collected_words[new_word] = i
-        new_word = [ ]
-      end
+  puts "STARTING LOOP"
+  while i <= characters.length 
+    word_build = characters.take(i).join
+        puts "BUILDING WORD"
+        if valid_word?(word_build) == true
+          collected_words << Hash[word_build => i]
+        end
+          
+    i += 1 
+    word_build =[ ]
   end
           
   return collected_words
@@ -23,24 +26,3 @@ end
 
 puts segment_string('ilovepuzzles')
 
-
-
-          if valid_word?(new_word.join) == false
-                danglers << new_word
-                  if danglers == true 
-                    skip_counter + 1
-                    erase the the dangling characters #slice?
-                    call the key of the last word
-                      while characters.length > i 
-                        new_word << characters.shift
-                          if valid_word?(new_word.join) == true
-                            collected_words.inject(new_word.join)
-                              if word is already in hash reset skip_counter 
-                              end
-                          end
-                      end
-                  end
-              end
-          end
-      end
-  end  
