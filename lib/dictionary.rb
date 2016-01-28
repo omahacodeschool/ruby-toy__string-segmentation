9d6c999f9a818a3c1ed2b8f0027e3bcfc702afdb
Jeremy Stoll <j8r8my@gmail.com>
2016-01-28 10:33:58 -0600

    Done

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,12 +1,5 @@
 def valid_word?(word)
-  # Populate this array with some words of your choosing.
-  dictionary = ["diamond", "blue", "bluest", "crystal", "quantity", "gorilla"]
+  dictionary = ["diamond", "blue", "bluest", "crystal", "quantity", "gorilla", "crystalize"]
+return (dictionary.grep(word).any?)
 
-  # Recieves possible word from the segmenter, and ...
-  # Checks word against each string in the dictionary.
-  
-  # Puts out T or F
-
-  # Return true if your dictionary contains `word`
-  # or false if it does not.
 end
--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -1,3 +1,45 @@
 def segment_string(str)
-  valid_word?(word)
-end
+    
+  str_arr = str.split(//)
+  resting_arr = []
+  words_arr = []
+  skip = 0
+  index_of_last_found_word = 0
+  9.times do
+       working_str_arr = str_arr[index_of_last_found_word..-1]
+        working_str_arr.each do |x|
+        resting_arr << x
+        
+          if 
+            valid_word?(resting_arr.join) && skip == 0
+            words_arr << resting_arr.join
+            resting_arr = []
+            
+          elsif 
+            valid_word?(resting_arr.join) && skip == 1
+            skip = 0
+            
+            
+          elsif 
+            valid_word?(resting_arr.join) == false
+         
+            
+          end
+        end
+       
+        
+        if 
+          resting_arr.empty? == true
+          return words_arr
+          
+        elsif 
+          resting_arr.empty? == false
+          resting_arr = []
+          skip = 1
+          words_arr.pop
+          index_of_last_found_word = words_arr.join.split(//).length 
+        end
+    end    
+end  
+
+
