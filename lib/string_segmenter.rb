3d5cb0cd3896486cf990678f01d4e56b18d8a1dc
Shannon Jackson <shannonfromomaha@gmail.com>
2016-01-28 09:41:42 -0600

    wip infinite loop

--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -8,44 +8,52 @@ def segment_string(str)
 
   char_evaluation = []
   possible_words = []
-  answer = []
+  leftovers = []
+  otherplace = []
   skip_counter = -1
 
   puts "About to loop..."
   until characters.empty?
-    char_evaluation << characters.shift
+    characters_clone = characters
+    leftovers << characters_clone.pop
+    char_evaluation = characters_clone
 
-    puts "Just shifted characters."
+    puts "Just deleted characters."
     puts "Now char_evaluation is #{char_evaluation}"
     puts "and characters is #{characters}"
+    puts "leftovers is #{leftovers}"
 
     test = char_evaluation.join
     if valid_word?(test) == true
      puts "THATS A WORD" 
       possible_words<< char_evaluation.join
-      char_evaluation = []
+      characters_clone=characters
     end
+    
+    until leftovers.empty?
+    characters << leftovers.pop
+    leftovers_clone = leftovers
+    otherplace << leftovers_clone.pop
+    char_evaluation = leftovers_clone
 
-    if valid_word?(test) == false && characters.empty?
-    puts "NOW THIS IS RELEVENT"
-      skip_counter = skip_counter + 1
-      x = skip_counter
-      until char_evaluation.empty?
-        puts "problem_word = #{possible_words[x]}"
-        tester_array = possible_words[x].dup
-        tester_array<< char_evaluation.shift
-        test = tester_array.to_s
-          if valid_word?(test) == true  
-            puts "FOUND YOU MOTHERFUCKER #{tester_array}"
-            possible_words[x] = tester_array
-            char_evaluation = []
-          else skip_counter = skip_counter + 1
-          end
-      
+    puts "Just deleted characters."
+    puts "Now char_evaluation is #{char_evaluation}"
+    puts "and characters is #{characters}"
+    puts "leftovers is #{leftovers}"
+
+    test = char_evaluation.join
+      if valid_word?(test) == true
+      puts "THATS A WORD" 
+      possible_words<< char_evaluation.join
+      characters_clone=characters
       end
     end
   end
+  
   puts possible_words.to_s
 end
 
-puts segment_string("catshitplaying")
+puts segment_string("playcatbutt")
+
+
+
