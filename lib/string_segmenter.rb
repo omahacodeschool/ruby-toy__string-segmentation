06a07e211dc84c54e41ce023143f9509a22a4f09
Derek Pressnall <derekpressnall@yahoo.com>
2016-01-28 14:11:18 -0600

    skip flag

--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -5,24 +5,41 @@ def segment_string(str)
   characters.length
   word_build = [ ]
   collected_words = [ ]
-  i = 0
-  #skip_counter = 0
 
-  puts "STARTING LOOP"
-  while i <= characters.length 
-    word_build = characters.take(i).join
-        puts "BUILDING WORD"
-        if valid_word?(word_build) == true
-          collected_words << Hash[word_build => i]
-        end
-          
-    i += 1 
-    word_build =[ ]
-  end
-          
-  return collected_words
+
+  start = 0
+  e = 0
+  skip_counter = 0
+  skip_flag = false
+  str = ''
+
   
+  while e <= characters.length 
+    word_build = characters[start..e].join
+      
+    if valid_word?(word_build) == true
+      if skip_flag == false
+        collected_words << word_build
+      else skip_flag == true
+      end
+      start = e + 1
+    else
+      
+      #new_diction << collected_words.to_s 
+      
+    end 
+    e += 1 #loop
+  end   
+  #when skip_flag == true
+      #redo 
+    #skip_counter +=  1
+  #if skip_counter == 13
+  #  skip_flag = true
+  #end
+
+  #if skip_flag
+
+  return collected_words
 end
 
-puts segment_string('ilovepuzzles')
-
+puts segment_string('frostbitehuman')
