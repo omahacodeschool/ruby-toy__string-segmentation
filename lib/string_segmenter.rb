318fc495c648a4af40714b7f17968d586aafcc0c
Chris Foral <chris.foral@gmail.com>
2016-01-29 14:49:01 -0600

    Success?

--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -21,29 +21,30 @@ i = 0
 this_many = 0
 str_loc = test_arr[0].length
 
-    while start_arr[str_loc..-1].length >= this_many
-      new_section = start_arr[str_loc..-1].take(this_many).join
+    if test_arr.length == 1
+        valid_arr << test_arr.shift
+    elsif test_arr.length >= 1
+        until test_arr.length == 1
+
+            while start_arr[str_loc..-1].length >= this_many
+                new_section = start_arr[str_loc..-1].take(this_many).join
       
-        if valid_word?(new_section) || test_arr.length == 1
-          valid_arr << test_arr.shift
+                if valid_word?(new_section)
+                valid_arr << test_arr.shift
+                break
+                end
+       
+                this_many += 1
+            end
+          valid_arr << test_arr.pop
+          test_arr.clear
           break
-        end
-        
-      this_many += 1
+        end 
     end
 
-# LAST PIECE: Compound words
-#
-# must figure out how to only shift test_arr when my 2nd while loop does a 
-# complete round for each element of test_arr
-
-  #test_arr.shift 
-  
     i = 0
     start_arr.shift(valid_arr[-1].length.to_i)
   end
 
   valid_arr
 end
-
-segment_string("speakthiswordordie")
