09a308d14c3a7caab9dac62f77e41e6ce4308fb8
Shannon Jackson <shannonfromomaha@gmail.com>
2016-01-28 19:10:19 -0600

    yeahyeahyeah

--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -1,10 +1,27 @@
 require_relative 'dictionary'
 
-def find_words(characters_arr)
+def segment_string(str)
+  characters = str.to_s.split("")
+  words = []
+  while characters.any?
+    puts "characters remaining: #{characters}"
+    leftovers,possible_words = find_words(words,characters)
+    characters = leftovers
+    puts "characters remaining: #{characters}"
+    if leftovers.any?
+      new_word, leftovers = leftovers_test(possible_words[-1],leftovers)
+      if new_word != nil
+        possible_words[-1] = new_word
+      end
+    end
+  end
+  puts possible_words.to_s
+end
+
+def find_words(words, characters)
   char_evaluation = []
   #array stores characters until they are established as words in dictionary
-  words = [] 
-  #array stores words that have been established in the string
+  characters_arr = characters.dup
   until characters_arr.empty? #until all the char are reviewed w no leftovers
     char_evaluation << characters_arr.shift #the letters are put one by one into array
     test1 = char_evaluation.join #a word is formed from the array
@@ -18,35 +35,21 @@ def find_words(characters_arr)
   return char_evaluation, words
 end
 
-def segment_string(str)
-  characters = str.to_s.split("")
-
-  leftovers,possible_words = find_words(characters)
-  
-  while leftovers.any?
-    puts "starting leftovers test"    
-    clone = leftovers.dup #makes a second list of leftover letters
-    possiblelongword = possible_words[-1].split("") #test last word 
-    until clone.empty? #until all the chars are reviewed 
-      possiblelongword << clone.shift
-      puts possiblelongword.join
-      gets
-      longwordtest = possiblelongword.join 
-      if valid_word?(longwordtest)
-        puts "found a word! #{longwordtest}"
-        possible_words[-1] = longwordtest
-        puts "words: #{possible_words} | leftovers: #{clone}"
-        leftovers = clone
-      end
+def leftovers_test(last_word,leftovers)
+  puts "starting leftovers test" 
+  possiblelongword = last_word.split("") #test last word 
+  until leftovers.empty? #until all the chars are reviewed 
+    possiblelongword << leftovers.shift
+    puts possiblelongword.join
+    longwordtest = possiblelongword.join 
+    if valid_word?(longwordtest)
+      puts "found a word! #{longwordtest}"
+      return longwordtest,leftovers
     end
-    leftovers,new_words = find_words(leftovers)
-    possible_words = possible_words + new_words
   end
-
-  puts possible_words.to_s
 end
 
-segment_string("catplayeringdog")
+segment_string("catshitplayeringdog")
 
 
 
