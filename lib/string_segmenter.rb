2cbc3ae1c96b3b572b8c100a30a706963a6abf0a
Beth Haubert <bethany.haubert@gmail.com>
2016-01-29 14:22:01 -0600

    created danglers? method

--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -12,14 +12,12 @@ def segment_string(str)
 	#create variables to save the index of the first letter currently in use and the index of the last letter currently in use
 	first_letter= 0
 	last_letter = 0
-	
+
 	#while the variable is less than or equal to the string length, do the following operation
 	while last_letter <= str.length do
 
-		#if the current collection of characters is not a word
-		#AND if the current value of last_letter is the same as the value of the length of the string
-		#AND if the length of the joined array is less than the length of the original string
-		if !valid_word?(str[first_letter..last_letter]) && last_letter == str.length && word_index.keys.join.length < str.length
+		if danglers?(str, first_letter, last_letter, word_index)
+		#if !valid_word?(str[first_letter..last_letter]) && last_letter == str.length && word_index.keys.join.length < str.length
 			#Take the value of the previous last letter and add one to it
 			last_letter = word_index.values.max + 1
 			#Delete the key/value pair with highest value
@@ -47,3 +45,52 @@ def segment_string(str)
 	end
 	return word_index.keys
 end
+
+
+###-------FIRST ATTEMPT AT REFACTORING--------###
+def danglers?(str, first_letter, last_letter, word_index)
+	!valid_word?(str[first_letter..last_letter]) && last_letter == str.length && word_index.keys.join.length < str.length
+end
+
+# Complete the following process when you find a word
+#
+# str - The String that is going to be acted on.
+#
+# Returns String and stores word in an Array and a Hash.
+# def post_word_finding_process(str, words, word_index, first_letter, last_letter)
+# 	words << str[first_letter..last_letter]
+# 	word_index[str[first_letter..last_letter]] = last_letter			
+# 	first_letter = last_letter + 1
+# 	last_letter += 1
+# end
+
+
+#Check for conditions in which there are danglers.
+#if the current collection of characters is not a word
+#AND if the current value of last_letter is the same as the value of the length of the string
+#AND if the length of the joined array is less than the length of the original string
+#
+# str - The String that is going to be acted on.
+#
+# Returns true or false.
+# def danglers(str, first_letter, last_letter, word_index)
+# 	!valid_word?(str[first_letter..last_letter]) && last_letter == str.length && word_index.join.length < str.length
+# end
+
+
+
+# Return to the beginning of the current word.
+#
+# str - The String that is going to be acted on.
+#
+# Returns String and removes word from Hash and Array.
+# def go_back_to_beginning_of_word(str)
+# 	last_letter = word_index.values.max + 1
+# 	words.delete(word_index.key(word_index.values.max))
+# 	word_index.delete(word_index.max_by{|k,v| v}[0])
+# 	if word_index.values.max == nil
+# 		first_letter = 0
+# 	else 
+# 		first_letter = word_index.values.max + 1 
+# 	end
+# end
