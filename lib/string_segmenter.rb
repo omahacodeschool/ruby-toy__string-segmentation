d8e46cbb699e539f243b0bd7a40474fbd3e3bda8
Derek Pressnall <derekpressnall@yahoo.com>
2016-01-29 14:58:13 -0600

    where I ended up stuck, some of sumeet code in #'s

--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -3,40 +3,36 @@ require_relative 'dictionary'
 def segment_string(str)
   characters = str.chars
   characters.length
-  word_build = [ ]
-  word_build_two = [ ]
-  word_build_three = [ ] 
-  collected_words = [ ]
+  #len = str.length
+  word_build = [ ] 
+  collected_words = {}
+
   start = 0
+  #i_last_real_word = 0
   e = 0
+  #i = 0
   skip_flag = false
   dangler = [ ]
-  word_join = ''
   #skip_counter = 0
 
   
   while e < characters.length 
     word_build = characters[start..e].join
+    #prefix = str[i_last_real_word...i]
       
     if valid_word?(word_build) == true
+    #if valid_word?(prefix)
       if skip_flag == false
-        collected_words << word_build
+      #if skip_counter > 0
+      #skip_counter -= 1
+        collected_words << Hash[word_build => e]
         start = e + 1
       end
-    else
-      dangler << word_build
     end  
     e += 1 #loop
   end   
-
-  word_build_two << dangler.pop
-  word_build_three << collected_words.pop
-  word_join = word_build_three.join + word_build_two.join  
-  
-  if valid_word?(word_join) == true
-    collected_words << word_join
-  end
   return collected_words
+
 end
 
 puts segment_string('ilovepuzzles')
