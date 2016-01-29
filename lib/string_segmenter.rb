3483d2adfb725d164d93ec075cb9f7029648387e
Shannon Jackson <shannonfromomaha@gmail.com>
2016-01-28 18:49:16 -0600

    letstrysomethingradical

--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -1,53 +1,49 @@
 require_relative 'dictionary'
 
-def segment_string(str)
-#establish the empty arrays
-  characters = str.to_s.split("")
-#array of string divided into individual characters
+def find_words(characters_arr)
   char_evaluation = []
-#array stores characters until they are established as words in dictionary
-  possible_words = []
-#array stores words that have been established in the string
-  leftovers = []
-#array stores any characters that have not been assigned..."danglers"
-  i = 0
-#establishes integer as 0--this is the skip counter for dummies
-
-#next if??????
-
-  until characters.empty? && leftovers.empty? #until all the char are reviewed w no leftovers
-
-    char_evaluation << characters.shift #the letters are put one by one into array
+  #array stores characters until they are established as words in dictionary
+  words = [] 
+  #array stores words that have been established in the string
+  until characters_arr.empty? #until all the char are reviewed w no leftovers
+    char_evaluation << characters_arr.shift #the letters are put one by one into array
     test1 = char_evaluation.join #a word is formed from the array
-    if valid_word?(test1) == true  #once the formed word matches dictionary
-    puts "found a word! #{test1}"
-     possible_words<< char_evaluation.join #that joined word is stored away
-     char_evaluation = [] #the evaluation array is emptied   
-     i = i+ 1 #the count of words goes up by one
-    end
-
-  leftovers = characters.dup
-
-    puts "initial leftovers = #{leftovers}"
-    if leftovers.any?  && i>0
-    puts "starting leftovers test"    
-      clone = leftovers.dup #makes a second list of leftover letters
-      possiblelongword = possible_words[-1].split("") #test last word 
-        until clone.empty? #until all the chars are reviewed 
-        possiblelongword << clone.shift 
-        longwordtest = possiblelongword.join 
-          if valid_word?(longwordtest) == true
-          possible_words[-1] = longwordtest
-          puts "at this point leftovers = #{leftovers}"
-          end
-        end
-      leftovers = clone
-      clone = []
-      puts "shitty leftovers = #{leftovers}" #this needs to NOT HAPPEN unless it's a new word??
+    if valid_word?(test1)  #once the formed word matches dictionary
+      puts "found a word! #{test1}"
+      words << test1 #that joined word is stored away
+      char_evaluation = [] #the evaluation array is emptied   
     end
   end
-puts i
-puts possible_words.to_s
+  puts "words: #{words} | leftovers: #{char_evaluation}"
+  return char_evaluation, words
+end
+
+def segment_string(str)
+  characters = str.to_s.split("")
+
+  leftovers,possible_words = find_words(characters)
+  
+  while leftovers.any?
+    puts "starting leftovers test"    
+    clone = leftovers.dup #makes a second list of leftover letters
+    possiblelongword = possible_words[-1].split("") #test last word 
+    until clone.empty? #until all the chars are reviewed 
+      possiblelongword << clone.shift
+      puts possiblelongword.join
+      gets
+      longwordtest = possiblelongword.join 
+      if valid_word?(longwordtest)
+        puts "found a word! #{longwordtest}"
+        possible_words[-1] = longwordtest
+        puts "words: #{possible_words} | leftovers: #{clone}"
+        leftovers = clone
+      end
+    end
+    leftovers,new_words = find_words(leftovers)
+    possible_words = possible_words + new_words
+  end
+
+  puts possible_words.to_s
 end
 
 segment_string("catplayeringdog")
