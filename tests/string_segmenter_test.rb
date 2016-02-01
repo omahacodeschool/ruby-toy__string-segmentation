1c6eb85a540b26f794afcb2db5fee5a88ba80435
Beth Haubert <bethany.haubert@gmail.com>
2016-02-01 17:13:14 -0600

    oop

--- tests/string_segmenter_test.rb
+++ tests/string_segmenter_test.rb
@@ -3,33 +3,38 @@ require 'string_segmenter'
 
 class StringSegmenterTest < Minitest::Test
   def test_ing_words
+    sample = SplitUpStringIntoWords.new
   	str = "catrunningtrance"
 
-  	assert_equal(["cat", "running", "trance"], segment_string(str))
+  	assert_equal(["cat", "running", "trance"], sample.segment_string(str))
   end
 
   def test_plural_words
+    sample = SplitUpStringIntoWords.new
   	str = "catsrunningtrance"
 
-  	assert_equal(["cats", "running", "trance"], segment_string(str))
+  	assert_equal(["cats", "running", "trance"], sample.segment_string(str))
   end
 
   def test_compoundish_words
+    sample = SplitUpStringIntoWords.new
   	str = "publishcatrun"
 
-  	assert_equal(["publish", "cat", "run"], segment_string(str))
+  	assert_equal(["publish", "cat", "run"], sample.segment_string(str))
   end
   
    def test_simple
+    sample = SplitUpStringIntoWords.new
   	str = "catrun"
 
-  	assert_equal(["cat", "run"], segment_string(str))
+  	assert_equal(["cat", "run"], sample.segment_string(str))
    end
 
    def test_complicated
+    sample = SplitUpStringIntoWords.new
   	str = "catsurethra"
 
-  	assert_equal(["cats", "urethra"], segment_string(str))
+  	assert_equal(["cats", "urethra"], sample.segment_string(str))
    end
 
 end
