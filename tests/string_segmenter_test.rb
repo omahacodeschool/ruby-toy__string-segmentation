83f681eca80e1e85e2cdd63843d778f9946a3936
Derek Pressnall <derekpressnall@yahoo.com>
2016-02-03 20:43:40 -0600

    OOP- no tomdoc

--- tests/string_segmenter_test.rb
+++ tests/string_segmenter_test.rb
@@ -1,25 +1,30 @@
 require 'test_helper'
-require 'string_segmenter'
+#require 'string_segmenter'
+require 'string_seg_sumeet2'
 
 class StringSegmenterTest < Minitest::Test
   def test_collect_words
     sample_str = "iloveyou"
-    assert_equal(["i", "love", "you"], segment_string(sample_str))
+    x = StringSegmentor.new
+    assert_equal(["i", "love", "you"], x.segment_string(sample_str))
   end
 
   def test_plural
     sample_str = "ilovepuzzles"
-    assert_equal(["i", "love", "puzzles"], segment_string(sample_str))
+    x = StringSegmentor.new
+    assert_equal(["i", "love", "puzzles"], x.segment_string(sample_str))
   end
 
   def test_compound
     sample_str = "frostbitehuman"
-    assert_equal(["frostbite", "human"], segment_string(sample_str))
+    x = StringSegmentor.new
+    assert_equal(["frostbite", "human"], x.segment_string(sample_str))
   end
 
   def test_word_in_word
     sample_str = "pressuremagicart"
-    assert_equal(["pressure", "magic", "art"], segment_string(sample_str))
+    x = StringSegmentor.new
+    assert_equal(["pressure", "magic", "art"], x.segment_string(sample_str))
   end
 
 end
